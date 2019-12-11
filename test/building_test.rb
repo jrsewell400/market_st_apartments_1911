5ca7e58b9c7c8eb8575d4dfdb3538000b30125e0
Jordan Sewell <jrsewell400@gmail.com>
2019-12-11 09:58:36 -0700

    Iteration 1 complete, start on Iteration 2

--- test/building_test.rb
+++ test/building_test.rb
@@ -7,7 +7,22 @@ require './lib/building'
 class BuildingTest < Minitest::Test
 
   def setup
-
+    @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
   end
 
+  def test_new_instances_exist
+    assert_instance_of Building, @building
+    assert_instance_of Apartment, @unit1
+    assert_instance_of Apartment, @unit2
+  end
+
+  def test_units_starts_as_empty_array
+    assert_equal [], @building.units
+  end
+
+  def test_units_can_be_added
+    
+  end
 end
